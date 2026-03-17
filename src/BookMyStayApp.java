a3ce300fffe99e05761b9312ab5612ba732936f3
chillsherlock <shubhampradhan806@gmail.com>
2026-03-17 14:35:11 +0530

    UC1: Application entry point

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,14 @@
+/**
+ * Book My Stay App
+ * UC1: Entry Point
+ * @version 1.0
+ */
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        System.out.println("==================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("==================================");
+    }
+}

56e8b78e6947b7b392b90a6903bd192bb9b1a6d3
chillsherlock <shubhampradhan806@gmail.com>
2026-03-17 14:47:59 +0530

    UC2: Room modeling using abstraction and inheritance

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -13,3 +13,76 @@ public class BookMyStayApp {
     }
 }
 
+/**
+ * Book My Stay App
+ * UC2: Room Modeling
+ * @version 2.0
+ */
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Book My Stay App - v2.0");
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        single.display();
+        System.out.println("Available: " + singleAvailable);
+
+        doubleRoom.display();
+        System.out.println("Available: " + doubleAvailable);
+
+        suite.display();
+        System.out.println("Available: " + suiteAvailable);
+    }
+}
+
+// Abstract class
+abstract class Room {
+    protected String type;
+    protected int price;
+
+    public Room(String type, int price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    public abstract void display();
+}
+
+// Child classes
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single", 1000);
+    }
+
+    public void display() {
+        System.out.println("Single Room - Price: " + price);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double", 2000);
+    }
+
+    public void display() {
+        System.out.println("Double Room - Price: " + price);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite", 5000);
+    }
+
+    public void display() {
+        System.out.println("Suite Room - Price: " + price);
+    }
+}
